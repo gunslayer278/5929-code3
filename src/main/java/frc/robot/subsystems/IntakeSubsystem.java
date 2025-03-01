0c41581ba779cdc07a59afcad7f79eac4e0bfded
Agame7k <comaidenaiden@gmail.com>
2025-03-01 13:09:25 -0600

    change

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,37 +1,36 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private SparkMax intakeMotor;
-    private SparkMaxConfig sparkMaxConfig;
+    private VictorSPX intakeMotor;
 
     public IntakeSubsystem() {
-        intakeMotor = new SparkMax(Constants.Intake.kIntakePort, MotorType.kBrushed);
+        intakeMotor = new VictorSPX(Constants.Intake.kIntakePort);
         
-        sparkMaxConfig = new SparkMaxConfig();
-        sparkMaxConfig.idleMode(IdleMode.kBrake);
-        intakeMotor.configure(sparkMaxConfig, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+        // Configure motor to brake mode instead of coast
+        intakeMotor.setNeutralMode(com.ctre.phoenix.motorcontrol.NeutralMode.Brake);
     }
 
     public void setIntakeSpeed(double speed) {
-        intakeMotor.set(speed);
+        intakeMotor.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, speed);
         SmartDashboard.putNumber("Intake Speed", speed);
     }
 
     public void stopIntake() {
-        intakeMotor.set(0);
+        intakeMotor.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, 0);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Intake Current", intakeMotor.getAppliedOutput());
+        // Victor SPX doesn't directly provide current measurement like SparkMax
+        // You would need a PDP/PDH to monitor current
+        SmartDashboard.putNumber("Intake Output", intakeMotor.get());
     }
 }
